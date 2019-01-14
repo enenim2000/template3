45a750539f6292da1aa178fc85416fd5a6d2fc3d
Asukwo Enenim <Asukwo.Enenim@etranzact.com>
2019-01-14 12:40:09 +0100

    updates

---
+++ js/config.js
@@ -0,0 +1,21 @@
+/**
+ * CONFIG PARAMETERS
+ */
+
+var APP_NAME = "CurexZone";
+var PHONE_NUMBER = "+44 7823 823067";
+
+
+
+
+
+
+
+
+
+/**
+ *  CONFIG PARAMETERS INITIALIZATION
+ */
+
+$(".app_name").text(APP_NAME);
+$(".phone_number").text(PHONE_NUMBER);
